72488dc168c842f34409fdd28f0d89d86bca0223
DennisWiseMawuliQuarshie-26 <quarshiedennis178@gmail.com>
2026-06-18 14:18:46 +0000

    Add files via upload

---
+++ Q. 9 Cpp.cpp
@@ -0,0 +1,10 @@
+//Assignment operator (=) is used to give a value to a variable.//
+
+int x = 5;
+
+//Equality operator (==) is used to compare two values.//
+
+if(x == 5)
+{
+    cout << "Equal";
+}
