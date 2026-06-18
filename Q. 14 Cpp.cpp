67d55f881393927ae300b4363ade2e09a3f650ca
DennisWiseMawuliQuarshie-26 <quarshiedennis178@gmail.com>
2026-06-18 11:54:37 +0000

    Add files via upload

---
+++ Q. 14 Cpp.cpp
@@ -0,0 +1,11 @@
+//Left Shift and Right Shift//
+
+//Left shift (<<) moves bits to the left//.
+
+int x = 5;
+cout << (x << 1);
+
+//Right shift (>>) moves bits to the right.//
+
+int x = 5;
+cout << (x >> 1);
