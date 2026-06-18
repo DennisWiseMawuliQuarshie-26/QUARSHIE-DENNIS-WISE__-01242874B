1fe32edb00426963f37c453d5bb8016a00fdebcb
DennisWiseMawuliQuarshie-26 <quarshiedennis178@gmail.com>
2026-06-18 14:15:45 +0000

    Add files via upload

---
+++ Q. 13 Cpp.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Q,D;
+    cin >> a >> b;
+
+    Q = Q ^ D;
+    D = Q ^ D;
+    Q = Q ^ D;
+
+    cout << Q << " " << D;
+
+    return 0;
+}
