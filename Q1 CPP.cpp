cea7bc413cb72045f5152c11350bd0b681f71baf
DennisWiseMawuliQuarshie-26 <quarshiedennis178@gmail.com>
2026-06-18 14:24:31 +0000

    Add files via upload

---
+++ Q1 CPP.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Q, D;
+    cout << "Enter two integers: ";
+    cin >> Q >> D;
+
+    cout << "Sum = " << Q + D << endl;
+    cout << "Difference = " << Q - D << endl;
+    cout << "Product = " << Q * D << endl;
+    cout << "Quotient = " << Q / D << endl;
+
+    return 0;
+}
